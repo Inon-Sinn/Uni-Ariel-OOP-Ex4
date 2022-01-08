e52655c49a1d8b08372fa0e8f472bc8952fb3f6f
Inon-Sinn <inon.sinn@gmail.com>
2022-01-08 18:53:19 +0200

    Pokemon Finder: Tested

--- Ex4_main.py
+++ Ex4_main.py
@@ -1,12 +1,24 @@
 
-
-
-# *********** main loop ************#
+from Model.GraphAlgo import GraphAlgo
 
 if __name__ == '__main__':
-    print("hi")
+    algo = GraphAlgo()
+    algo.load_from_json("data/A0")
+    pos = (35.195224052340706,32.10575624080796)
+    print(algo.closestEdges(pos))
+    distances = algo.closestEdges(pos)
+    distances = sorted(distances, key=lambda x: x[2])
+    print(distances)
+    print(algo.edgeByType(-1, distances))
+    print(algo.PokemonPlacement(-1, pos))
 
 
+# "data/A0"
 
+# pos='35.197656770719604,32.10191878639921,0.0' - (9,8)
 
+# 35.199963710098416,32.105723673136964 - (4,3)
 
+# 35.195224052340706,32.10575624080796 - (3,2)
+
+# 35.19494883961552,32.105809680537625 - (3,2)
