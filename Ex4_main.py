97a616a95053b976a5de8107d3dacdc55047e2e0
Inon-Sinn <inon.sinn@gmail.com>
2022-01-09 18:06:58 +0200

    finshed

--- Ex4_main.py
+++ Ex4_main.py
@@ -2,4 +2,4 @@ from GUI import Gui, WIDTH, HEIGHT
 
 if __name__ == '__main__':
     """Run the Gui after turning on the server"""
-    test = Gui(WIDTH, HEIGHT, debug=False)
+    test = Gui(WIDTH, HEIGHT, debug=True)
